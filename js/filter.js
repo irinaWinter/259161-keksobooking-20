3c44b2e79b2a240cbed337e378d9c8a4d4e204c1
Itina Winter <lobanova.irina.96@mail.ru>
2020-06-27 14:13:03 +0300

    Начинает реализацию фильтра

--- js/filter.js
+++ js/filter.js
@@ -2,23 +2,65 @@
 
 (function () {
   var type = document.querySelector('#housing-type');
+  // Price
+  var rooms = document.querySelector('#housing-rooms');
+  var guests = document.querySelector('#housing-guests');
+  // Features
 
-  var updateAds = function () {
+  var getRank = function (ad) {
+    var rank = 0;
 
-    var sameType = window.map.ads.filter(function (it) {
-      if (type.value === 'any') {
-        return it.offer.type !== type.value;
-      }
+    if (ad.offer.type === type.value) {
+      rank += 1;
+    }
 
-      return it.offer.type === type.value;
-    });
-    window.card.removeAdCard();
-    window.map.addPins(sameType);
+    // Price
+
+    if (ad.offer.rooms === +rooms.value) {
+      rank += 1;
+    }
+
+    if (ad.offer.guests === +guests.value) {
+      rank += 1;
+    }
+
+    // Features
+
+    return rank;
   };
 
-  var typeChangeHandler = function () {
+  var total = 0;
+
+  var changeTotal = function (evt) {
+    var currentValue;
+
+    if (evt.target.value !== 'any' && (currentValue === 'any' || !currentValue)) {
+      total++;
+    } else if (evt.target.value !== 'any' && currentValue !== 'any') {
+      total = total;
+    } else if (evt.target.value === 'any' && evt.target.value !== currentValue) {
+      total--;
+    }
+    currentValue = evt.target.value;
+  };
+
+  var compareRank = function (it) {
+    return getRank(it) === total;
+  };
+
+  var updateAds = function () {
+    var filteredAds = window.map.ads.filter(compareRank);
+    console.log(filteredAds);
+
+    window.card.removeAdCard();
+    window.map.addPins(filteredAds);
+  };
+
+  var filterChangeHandler = function (evt) {
+    changeTotal(evt);
     updateAds();
   };
 
-  type.addEventListener('change', typeChangeHandler);
+  var filter = document.querySelector('.map__filters');
+  filter.addEventListener('change', filterChangeHandler);
 })();
